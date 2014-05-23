2e464fe83f8e604a3976f11cc65c8406f86c438f
Alxandr <alxandr@alxandr.me>
2014-05-23 02:51:43 +0200

    Added a sample test

---
+++ test/Sample.Tests/SampleTest.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace Sample.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void True_is_true()
+        {
+            Assert.True(true);
+        }
+    }
+}
